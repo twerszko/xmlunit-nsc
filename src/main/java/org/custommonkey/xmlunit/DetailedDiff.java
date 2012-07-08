37421afc9afe430858ced4e5e5509b2e3cef7cc6
twerszko <twerszko@gmail.com>
2012-07-08 16:55:32 +0200

    DifferenceTest refactored.

--- src/main/java/org/custommonkey/xmlunit/DetailedDiff.java
+++ src/main/java/org/custommonkey/xmlunit/DetailedDiff.java
@@ -1,5 +1,5 @@
 /*
-******************************************************************
+ ******************************************************************
 Copyright (c) 2001-2008, Jeff Martin, Tim Bacon
 All rights reserved.
 
@@ -7,13 +7,13 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
 
-    * Redistributions of source code must retain the above copyright
+ * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
+ * Redistributions in binary form must reproduce the above
       copyright notice, this list of conditions and the following
       disclaimer in the documentation and/or other materials provided
       with the distribution.
-    * Neither the name of the xmlunit.sourceforge.net nor the names
+ * Neither the name of the xmlunit.sourceforge.net nor the names
       of its contributors may be used to endorse or promote products
       derived from this software without specific prior written
       permission.
@@ -31,84 +31,91 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 
-******************************************************************
-*/
+ ******************************************************************
+ */
 
 package org.custommonkey.xmlunit;
 
 import java.util.List;
-import java.util.ArrayList;
+
+import com.google.common.collect.Lists;
 
 /**
- * Compares and describes all the differences between two XML documents.
- * The document comparison does not stop once the first unrecoverable difference
- * is found, unlike the Diff class.
- * Note that because the differences are described relative to some control XML
- * the list of all differences when <i>A</i> is compared to <i>B</i> will not
- * necessarily be the same as when <i>B</i> is compared to <i>A</i>.
- * <br />Examples and more at <a href="http://xmlunit.sourceforge.net"/>xmlunit.sourceforge.net</a>
+ * Compares and describes all the differences between two XML documents. The
+ * document comparison does not stop once the first unrecoverable difference is
+ * found, unlike the Diff class. Note that because the differences are described
+ * relative to some control XML the list of all differences when <i>A</i> is
+ * compared to <i>B</i> will not necessarily be the same as when <i>B</i> is
+ * compared to <i>A</i>. <br />
+ * Examples and more at <a
+ * href="http://xmlunit.sourceforge.net"/>xmlunit.sourceforge.net</a>
  */
 public class DetailedDiff extends Diff {
-    private final List allDifferences;
+	private final List<Difference> allDifferences;
 
-    /**
-     * Create a new instance based on a prototypical Diff instance
-     * @param prototype the Diff instance for which more detailed difference
-     *  information is required
-     */
-    public DetailedDiff(Diff prototype) {
-        super(prototype);
-        allDifferences = new ArrayList();
-    }
+	/**
+	 * Create a new instance based on a prototypical Diff instance
+	 * 
+	 * @param prototype
+	 *            the Diff instance for which more detailed difference
+	 *            information is required
+	 */
+	public DetailedDiff(Diff prototype) {
+		super(prototype);
+		allDifferences = Lists.newArrayList();
+	}
 
-    /**
-     * DifferenceListener implementation.
-     * Add the difference to the list of all differences
-     * @param expected
-     * @param actual
-     * @param control
-     * @param test
-     * @param comparingWhat
-     * @return the value supplied by the superclass implementation
-     */
-    public int differenceFound(Difference difference) {
-        final int returnValue = super.differenceFound(difference);
-        switch (returnValue) {
-        case RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL:
-            return returnValue;
-        case RETURN_ACCEPT_DIFFERENCE:
-            break;
-        case RETURN_IGNORE_DIFFERENCE_NODES_SIMILAR:
-            difference.setRecoverable(true);
-            break;
-        case RETURN_UPGRADE_DIFFERENCE_NODES_DIFFERENT:
-            difference.setRecoverable(false);
-            break;
-        default:
-            throw new IllegalArgumentException(returnValue
-                                               + " is not a defined "
-                                               + " DifferenceListener"
-                                               + ".RETURN_... value");
-        }
-        allDifferences.add(difference);
-        return returnValue;
-    }
+	/**
+	 * DifferenceListener implementation. Add the difference to the list of all
+	 * differences
+	 * 
+	 * @param expected
+	 * @param actual
+	 * @param control
+	 * @param test
+	 * @param comparingWhat
+	 * @return the value supplied by the superclass implementation
+	 */
+	public int differenceFound(Difference difference) {
+		final int returnValue = super.differenceFound(difference);
+		switch (returnValue) {
+		case RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL:
+			return returnValue;
+		case RETURN_ACCEPT_DIFFERENCE:
+			break;
+		case RETURN_IGNORE_DIFFERENCE_NODES_SIMILAR:
+			difference.setRecoverable(true);
+			break;
+		case RETURN_UPGRADE_DIFFERENCE_NODES_DIFFERENT:
+			difference.setRecoverable(false);
+			break;
+		default:
+			throw new IllegalArgumentException(returnValue
+			        + " is not a defined "
+			        + " DifferenceListener"
+			        + ".RETURN_... value");
+		}
+		allDifferences.add(difference);
+		return returnValue;
+	}
 
-    /**
-     * ComparisonController implementation.
-     * @param afterDifference
-     * @return false always as this class wants to see all differences
-     */
-    public boolean haltComparison(Difference afterDifference) {
-        return false;
-    }
+	/**
+	 * ComparisonController implementation.
+	 * 
+	 * @param afterDifference
+	 * @return false always as this class wants to see all differences
+	 */
+	public boolean haltComparison(Difference afterDifference) {
+		return false;
+	}
 
-    /**
-     * Obtain all the differences found by this instance
-     * @return a list of {@link Difference differences}
-     */
-    public List getAllDifferences() {
-        compare();
-        return allDifferences;
-    }
+	/**
+	 * Obtain all the differences found by this instance
+	 * 
+	 * @return a list of {@link Difference differences}
+	 */
+	public List<Difference> getAllDifferences() {
+		compare();
+		return allDifferences;
+	}
 }
---
+++ src/test/java/org/custommonkey/xmlunit/DifferenceTest.java
@@ -0,0 +1,161 @@
+/*
+ ******************************************************************
+Copyright (c) 2001-2010, Jeff Martin, Tim Bacon
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+ * Neither the name of the xmlunit.sourceforge.net nor the names
+      of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+ ******************************************************************
+ */
+
+package org.custommonkey.xmlunit;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+/**
+ * @author TimBacon
+ */
+public class DifferenceTest {
+	private final Difference ORIGINAL = DifferenceConstants.ATTR_NAME_NOT_FOUND;
+
+	@Test
+	public void should_check_defensive_constructor() {
+		// given
+		Difference difference = new Difference(ORIGINAL, null, null);
+
+		// then
+		assertThat(difference.getId()).isEqualTo(ORIGINAL.getId());
+		assertThat(difference.getDescription()).isEqualTo(ORIGINAL.getDescription());
+		assertThat(difference.isRecoverable()).isEqualTo(ORIGINAL.isRecoverable());
+
+		assertThat(ORIGINAL.isRecoverable()).isFalse();
+		difference.setRecoverable(true);
+		assertThat(ORIGINAL.isRecoverable()).isNotEqualTo(difference.isRecoverable());
+	}
+
+	@Test
+	public void should_check_equals() {
+		// given
+		Difference difference = new Difference(ORIGINAL, null, null);
+
+		// then
+		assertThat(difference).isNotEqualTo(null);
+		assertFalse(difference.equals("aString"));
+		assertThat(difference).isEqualTo(ORIGINAL);
+		assertThat(difference).isEqualTo(difference);
+	}
+
+	@Test
+	public void should_check_toString() {
+		// given
+		Difference attrNameNotFound = DifferenceConstants.ATTR_NAME_NOT_FOUND;
+
+		String originalAsString =
+		        "Difference (#" + attrNameNotFound.getId() + ") " +
+		                attrNameNotFound.getDescription();
+
+		// then
+		assertThat(attrNameNotFound.toString()).isEqualTo(originalAsString);
+	}
+
+	@Test
+	public void should_check_toString2() {
+		// given
+		Document document = XMLUnit.newControlParser().newDocument();
+		Node controlNode = document.createComment("control");
+		NodeDetail controlNodeDetail = new NodeDetail(
+		        controlNode.getNodeValue(),
+		        controlNode,
+		        "/testToString/comment()");
+		Node testNode = document.createComment("test");
+		NodeDetail testNodeDetail = new NodeDetail(
+		        testNode.getNodeValue(),
+		        testNode,
+		        "/testToString/comment()");
+
+		Difference difference = new Difference(
+		        DifferenceConstants.COMMENT_VALUE,
+		        controlNodeDetail,
+		        testNodeDetail);
+		StringBuffer buf = new StringBuffer("Expected ")
+		        .append(DifferenceConstants.COMMENT_VALUE.getDescription())
+		        .append(" 'control' but was 'test' - comparing ");
+
+		NodeDescriptor.appendNodeDetail(buf, controlNodeDetail);
+		buf.append(" to ");
+
+		NodeDescriptor.appendNodeDetail(buf, testNodeDetail);
+
+		// then
+		assertThat(difference.toString()).isEqualTo(buf.toString());
+	}
+
+	// bug 2386807
+	@Test
+	public void should_check_xpath_of_missing_attribute() throws SAXException, IOException {
+		// given
+		String control = "<foo><bar a=\"x\" y=\"z\"/></foo>";
+		String test = "<foo><bar a=\"x\"/></foo>";
+		Diff diff = new Diff(control, test);
+		DetailedDiff detailedDiff = new DetailedDiff(diff);
+
+		// when
+		List<Difference> diffs = detailedDiff.getAllDifferences();
+
+		Difference difference1 = diffs.get(0);
+		String controlXpathLocation1 = difference1.getControlNodeDetail().getXpathLocation();
+		String testXpathLocation1 = difference1.getTestNodeDetail().getXpathLocation();
+
+		Difference difference2 = diffs.get(1);
+		String controlXpathLocation2 = difference2.getControlNodeDetail().getXpathLocation();
+		String testXpathLocation2 = difference2.getTestNodeDetail().getXpathLocation();
+
+		// then
+		assertThat(diffs).hasSize(2);
+
+		assertThat(difference1.getId())
+		        .isEqualTo(DifferenceConstants.ELEMENT_NUM_ATTRIBUTES_ID);
+		assertThat(controlXpathLocation1).isEqualTo("/foo[1]/bar[1]");
+		assertThat(testXpathLocation1).isEqualTo("/foo[1]/bar[1]");
+
+		assertThat(difference2.getId())
+		        .isEqualTo(DifferenceConstants.ATTR_NAME_NOT_FOUND_ID);
+		assertThat(controlXpathLocation2).isEqualTo("/foo[1]/bar[1]/@y");
+		assertThat(testXpathLocation2).isEqualTo("/foo[1]/bar[1]");
+	}
+}
