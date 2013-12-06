aa728780a4f8bb29fe4b23592ae07f02ff65fa56
tsayen <anatoly.sayenko@gmail.com>
2013-12-06 12:23:43 +0100

    finish refactoring of DiffTest

--- src/test/java/org/custommonkey/xmlunit/diff/DiffTest.java
+++ src/test/java/org/custommonkey/xmlunit/diff/DiffTest.java
@@ -36,47 +36,44 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package org.custommonkey.xmlunit.diff;
 
+import static org.custommonkey.xmlunit.diff.Diff.newDiff;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.custommonkey.xmlunit.diff.Diff.newDiff;
-import static org.custommonkey.xmlunit.diff.Diffs.*;
-
-import java.util.List;
-
+import static org.mockito.Mockito.verify;
 import net.sf.xmlunit.diff.Comparison;
 import net.sf.xmlunit.diff.ComparisonListener;
 import net.sf.xmlunit.diff.ComparisonResult;
 import net.sf.xmlunit.diff.DefaultDifferenceEngineFactory;
 import net.sf.xmlunit.diff.DifferenceEngine;
+import net.sf.xmlunit.diff.DifferenceEngineFactory;
 import net.sf.xmlunit.diff.ListingDifferenceEvaluator;
 
 import org.custommonkey.xmlunit.util.DocumentUtils;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+
 
 public class DiffTest extends DiffTestAbstract {
     @Test
     public void should_stop_comparison_after_first_difference() throws Exception {
         // given
-        DocumentUtils utils = new DocumentUtils(properties);
-        Document controlDoc = utils.newControlDocumentBuilder().newDocument();
-        Element control = controlDoc.createElement("foo");
-        controlDoc.appendChild(control);
-        control.appendChild(controlDoc.createTextNode("text"));
-
-        Document testDoc = utils.newTestDocumentBuilder().newDocument();
-        Element test = testDoc.createElement("bar");
-        testDoc.appendChild(test);
-
-        // when
-        final ComparisonListener listener = mock(ComparisonListener.class);
-
         ListingDifferenceEvaluator evaluator = new ListingDifferenceEvaluator();
-        DefaultDifferenceEngineFactory engineFactory = new DefaultDifferenceEngineFactory(properties) {
+        ComparisonListener listener = mock(ComparisonListener.class);
+        Diff diff = newDiff().betweenControlDocument(buildControlDocument()).andTestDocument(buildTestDocument()).build();
+        diff.setEngineFactory(createEngineFactory(evaluator, listener));
+        // when
+        boolean identical = diff.identical();
+        // then
+        assertThat(evaluator.getDifferences()).hasSize(1);
+        assertThat(identical).isFalse();
+        verify(listener, times(1)).comparisonPerformed(any(Comparison.class), any(ComparisonResult.class));
+    }
+
+    private DifferenceEngineFactory createEngineFactory(ListingDifferenceEvaluator evaluator,
+            final ComparisonListener listener) {
+        DifferenceEngineFactory engineFactory = new DefaultDifferenceEngineFactory(properties) {
             @Override
             public DifferenceEngine newEngine() {
                 DifferenceEngine engine = super.newEngine();
@@ -85,17 +82,18 @@ public class DiffTest extends DiffTestAbstract {
             }
         };
         engineFactory.useEvaluator(evaluator);
+        return engineFactory;
+    }
 
-        Diff diff = newDiff().betweenControlDocument(controlDoc).andTestDocument(testDoc).build();
-        diff.setEngineFactory(engineFactory);
-        boolean identical = diff.identical();
+    Document buildTestDocument() {
+        Document doc = new DocumentUtils().newTestDocumentBuilder().newDocument();
+        doc.appendChild(doc.createElement("bar"));
+        return doc;
+    }
 
-        List<Comparison> differences = evaluator.getDifferences();
-
-        // then
-        assertThat(differences).hasSize(1);
-        assertThat(identical).isFalse();
-        Mockito.verify(listener, times(1)).comparisonPerformed(any(Comparison.class), any(ComparisonResult.class));
-
+    private Document buildControlDocument() {
+        Document doc = new DocumentUtils().newControlDocumentBuilder().newDocument();
+        doc.appendChild(doc.createElement("foo")).appendChild(doc.createTextNode("foo text"));
+        return doc;
     }
 }
